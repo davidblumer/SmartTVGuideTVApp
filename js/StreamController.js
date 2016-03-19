49da72bae41f98f2646dcdd2b19ec82f6b7fb4e1
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2016-03-19 15:34:53 +0100

    Angular works

---
+++ js/StreamController.js
@@ -0,0 +1,13 @@
+/* global app */
+angular.module('smartGuide').controller('StreamController', function (
+      $log
+    , $rootScope
+    , $scope
+)
+{
+    $scope.entries = [1, 2, 3];
+
+
+
+    
+});
